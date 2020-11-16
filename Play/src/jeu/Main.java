f6869d1ef78c2d74dbb6e0af1d7ee0917d1b20c9
unknown <lioret.guillaume@gmail.com>
2020-11-16 12:17:03 +0100

    Classe labyrinthe avec création lab vide

--- Play/src/jeu/Main.java
+++ Play/src/jeu/Main.java
@@ -6,7 +6,10 @@ public class Main {
 		// TODO Auto-generated method stub
 Monstre Ted=new Monstre();
 Ted.deplacement();
+Labyrinthe test=new Labyrinthe(4,6);
+int[][] a=test.creer_lab();
+for (int i=0;i<a.length;i++) {
+	for (int j=0;j<a[i].length;j++)
+	{System.out.println(a[i][j]);}}
 
-	}
-
-}
+}}
