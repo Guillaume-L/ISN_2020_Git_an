fc0c5f538010fc55d0ae35ff61663ca52b9478ed
Guillaume <lioret.guillaume@gmail.com>
2020-11-16 12:21:42 +0100

    ajout labyrinthe

--- Play/src/jeu/Main.java
+++ Play/src/jeu/Main.java
@@ -8,8 +8,10 @@ Monstre Ted=new Monstre();
 Ted.deplacement();
 Labyrinthe test=new Labyrinthe(4,6);
 int[][] a=test.creer_lab();
+
 for (int i=0;i<a.length;i++) {
+	System.out.println(" ");
 	for (int j=0;j<a[i].length;j++)
-	{System.out.println(a[i][j]);}}
+	{System.out.print(a[i][j]);}}
 
 }}
