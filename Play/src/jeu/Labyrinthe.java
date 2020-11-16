ab4a4b00290f7feceb1a2048350d878d6afd1a7b
Guillaume <lioret.guillaume@gmail.com>
2020-11-16 12:22:30 +0100

    ajout labyrinthe

---
+++ Play/src/jeu/Labyrinthe.java
@@ -0,0 +1,25 @@
+package jeu;
+
+public class Labyrinthe {int longueur; int largeur;
+
+Labyrinthe(int plongueur,int plargeur) {longueur=plongueur;largeur=plargeur;}
+
+public int[][] creer_lab(){
+	int[][] lab=new int[longueur][largeur];
+	for (int i=0;i<lab.length;i++) {
+		for (int j=0;j<lab[i].length;j++)
+		{if (i==0 || j==0 || i==lab.length-1 || j==lab[i].length-1)
+{lab[i][j]=1;
+		}}}
+	
+	return lab;
+}
+}
+
+
+	
+	
+	
+
+
+
