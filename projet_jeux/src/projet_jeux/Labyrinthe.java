1d183ee13136212feeeebb20c67f2a616a8d53e0
Pierre-Louis COLLIN <pierrelouiscollin@outlook.fr>
2020-11-18 12:38:10 +0100

    Ajout classe Jeu et transfert méthodes de la classe labyrinthe vers la classe Jeu

--- projet_jeux/src/projet_jeux/Labyrinthe.java
+++ projet_jeux/src/projet_jeux/Labyrinthe.java
@@ -90,106 +90,13 @@ public class Labyrinthe {
 		this.positionTresor = positionTresor;
 		this.cases[longueur -2][largeur -2] = new Tresor(false, true);
 	}
-	public void affichage() {
-		for (int i=0;i<this.largeurY;i++) {
-			System.out.println(" ");
-			for (int j=0;j<this.longueurX;j++) {
-				if (this.cases[i][j].testPresence()) {
-					System.out.print(this.cases[i][j].population.get(0).visuel+" ");
-				}
-				else {
-					System.out.print(this.cases[i][j].visuel+" ");
-				}
-			}
-		}
-	}
-	public void peuplement(Monstre monstre) {
-		this.cases[monstre.positionY][monstre.positionX].ajoutPersonnage(monstre);
-		this.populationMonstre.add(monstre);
-	}
-	public void peuplement(Hero hero) {
-		this.cases[hero.positionY][hero.positionX].ajoutPersonnage(hero);
-		this.hero = hero;
-	}
-	public void randomPlacementMonstre(int nbMonstre) {
-		while (nbMonstre >0) {
-			int positionX = (int)(Math.random() * (longueurX-2)) + 1;
-			int positionY = (int)(Math.random() * (largeurY-2)) + 1;
-			Monstre monstre = new Monstre(1, positionX,positionY);
-			if (!(this.cases[positionX][positionY].testPresence())) {
-				this.peuplement(monstre);
-				nbMonstre = nbMonstre - 1;
-			}
-		}
-	}
-	public void deplacement(Monstre monstre) {
-		boolean recherche = true;
-		int i = 0;
-		while (recherche) {
-			if (this.populationMonstre.get(i).equals(monstre)) {
-			}
-		}
-	}
-	public void deplacementHero(String mouvement) {
-		switch (mouvement) {
-			case "z":
-				if (this.cases[this.hero.positionY -1][this.hero.positionX].franchissable) {
-					this.cases[this.hero.positionY][this.hero.positionX].retirerHero();
-					this.hero.positionY -= 1;
-					this.cases[this.hero.positionY][this.hero.positionX].ajoutPersonnage(this.hero);
-				}
-				else {
-					System.out.println("Le héro bloque contre un mur");
-				}
-			break;
-			case "s":
-				if (this.cases[this.hero.positionY +1][this.hero.positionX].franchissable) {
-					this.cases[this.hero.positionY][this.hero.positionX].retirerHero();
-					this.hero.positionY += 1;
-					this.cases[this.hero.positionY][this.hero.positionX].ajoutPersonnage(this.hero);
-				}
-				else {
-					System.out.println("Le héro bloque contre un mur");
-				}
-			break;
-			case "q":
-				if (this.cases[this.hero.positionY][this.hero.positionX -1].franchissable) {
-					this.cases[this.hero.positionY][this.hero.positionX].retirerHero();
-					this.hero.positionX -= 1;
-					this.cases[this.hero.positionY][this.hero.positionX].ajoutPersonnage(this.hero);
-				}
-				else {
-					System.out.println("Le héro bloque contre un mur");
-				}
-			break;
-			case "d":
-				if (this.cases[this.hero.positionY][this.hero.positionX +1].franchissable) {
-					this.cases[this.hero.positionY][this.hero.positionX].retirerHero();
-					this.hero.positionX += 1;
-					this.cases[this.hero.positionY][this.hero.positionX].ajoutPersonnage(this.hero);
-				}
-				else {
-					System.out.println("Le héro bloque contre un mur");
-				}
-			break;
-			default:
-		}
-	}
+	
 	public void deplacementTresor(int positionX, int positionY) {
 		this.cases[positionTresor[0]][positionTresor[1]] = new Sol();
 		this.cases[positionX][positionY] = new Tresor(false, true);
 		positionTresor[0] = positionX;
 		positionTresor[1] = positionY;
 	}
-	public boolean testVictoire() {
-		return ((this.hero.positionX == this.positionTresor[0]) && (this.hero.positionY == this.positionTresor[1]));
-	}
-	public void resoudreCombat() {
-		for (Monstre monstre : this.populationMonstre) {
-			if ((this.hero.positionX == monstre.positionX) && (this.hero.positionY == monstre.positionY)) {
-				this.hero.subirDegats();
-			}
-		}
-	}
+
 
 }
--- projet_jeux/src/projet_jeux/Main.java
+++ projet_jeux/src/projet_jeux/Main.java
@@ -6,21 +6,20 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		boolean victoire = false;
 		Labyrinthe labyrinthe = new Labyrinthe(10,10);
 		Hero hero = new Hero(1,1,1);
-		labyrinthe.peuplement(hero);
 		labyrinthe.deplacementTresor(8, 8);
-		labyrinthe.randomPlacementMonstre(5);
+		Jeu jeu = new Jeu(labyrinthe, hero);
+		jeu.randomPopulationMonstre(5);
 		Scanner scan = new Scanner(System.in);
-		while ((!labyrinthe.testVictoire()) && (labyrinthe.hero.testVivant())) {
-			labyrinthe.affichage();
+		while ((!jeu.testVictoire()) && (jeu.hero.testVivant())) {
+			jeu.affichage();
 			System.out.println("Deplacement z/q/s/d");
 			String commande = scan.nextLine(); // Lire la saisie de l'utilisateur
-			labyrinthe.deplacementHero(commande);
-			labyrinthe.resoudreCombat();
+			jeu.deplacementHero(commande);
+			jeu.resoudreCombat();
 		}
-		if (labyrinthe.testVictoire()) {
+		if (jeu.testVictoire()) {
 			System.out.println("Victoire : Trésor récupéré !");
 		}
 		else {System.out.println("Défaite : héro tué par un monstre");
