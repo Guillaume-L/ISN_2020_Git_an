e4b734aff1854658bb21c7203949d1862749d5f5
Stan8-git <stanlois.riviere97417@gmail.com>
2020-11-21 17:22:10 +0100

    Classe Personnage

--- Play/src/jeu/Personnage.java
+++ Play/src/jeu/Personnage.java
@@ -1,9 +1,20 @@
 package jeu;
 
 public abstract class Personnage {
+	private int point_de_vie; 
+	private int position_x;
+	private int position_y;
 	
-	
-	
-public void deplacement() {System.out.println("Méthode à définir");}
-
+	public boolean deplacement(Labyrinthe L, int x, int y) {
+		if (((Math.abs(x-position_x)!=1) && (Math.abs(y-position_y)!=0)) || 
+				((Math.abs(x-position_x)!=0) && (Math.abs(y-position_y)!=1))) {
+			return false;	
+		}
+		else if (x>L.longueur || y>L.largeur) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 }
