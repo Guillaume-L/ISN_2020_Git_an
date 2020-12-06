58b09eec00d38c3a731c6dd182f02ae3f65bb5cd
Guillaume <lioret.guillaume@gmail.com>
2020-12-06 15:29:25 +0100

    Guillaume deplacement ennemi

--- Play/src/jeu/Monstre.java
+++ Play/src/jeu/Monstre.java
@@ -1,5 +1,6 @@
 package jeu;
-
+import java.util.ArrayList;
+import java.util.Random;
 public class Monstre extends Personnage{
 
 	public Monstre(int point_de_vie, int position_x, int position_y) {
@@ -9,6 +10,42 @@ public class Monstre extends Personnage{
 		this.visuel = "E";
 		this.image = "monstre";
 	}
+	
+public void deplacement(Labyrinthe L) {
+	boolean deplac;
+	int[] coord=new int[2];
+	ArrayList<int[]> deplacement_possib= new ArrayList<int[]>();
+	int x=this.position_x+1;
+	int y=this.position_y;
+	coord[0]=x;
+	coord[1]=y;
+	deplac=this.test_deplacement(L,x,y);
+	if (deplac) {deplacement_possib.add(coord);}
+	 x=this.position_x-1;
+	 y=this.position_y;
+	coord[0]=x;
+	coord[1]=y;
+	deplac=this.test_deplacement(L,x,y);
+	if (deplac) {deplacement_possib.add(coord);}
+	 x=this.position_x;
+	 y=this.position_y+1;
+	coord[0]=x;
+	coord[1]=y;
+	deplac=this.test_deplacement(L,x,y);
+	if (deplac) {deplacement_possib.add(coord);}
+	 x=this.position_x+1;
+	 y=this.position_y-1;
+	coord[0]=x;
+	coord[1]=y;
+	deplac=this.test_deplacement(L,x,y);
+	if (deplac) {deplacement_possib.add(coord);}
+	int taille=deplacement_possib.size();
+	Random rand= new Random();
+	int[] move=deplacement_possib.get(rand.nextInt(taille));
+	this.position_x=move[0];
+	this.position_y=move[1];
+	
+}
 	
 
 }
